0c380f1bbfdc20cd046fd5a7220f3bc2798db1fa
mai-sami <mai.jaber@hotmail.com>
2023-08-25 16:59:05 +0300

    fixed activeClassName

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,8 +4,12 @@ import { BsFillAirplaneFill } from "react-icons/bs";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import { Divider } from "@mui/material";
- 
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+
 const Sidebar = () => {
+  const { pathname } = useLocation();
+
   const [open, setOpen] = React.useState(true);
 
   const handleClick = () => {
@@ -15,36 +19,43 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="sidebar_item">
         <BsFillAirplaneFill className="icon" />
-        <div className="item_tite">Dashboard</div>
-      </div>
+             <NavLink
+              exact
+              activeClassName={"now"}
+              className={pathname === "/" ? "Link now" : "Link"}
+              to="/"
+              >
+              <div className="item_tite" >
+              Dashboard
+          </div>
+            </NavLink>
+       </div>
       <div className="sidebar_item">
         <BsFillAirplaneFill className="icon" />
         <div className="item_tite space">
           <div>Users</div>
           <div onClick={handleClick}>
-          {open ?  <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
-
+            {open ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
           </div>
-         </div>
         </div>
+      </div>
 
-        {open ?  null: 
-          <div className="sidebar_inside">
-            <div className="sidebar_item">
-              <BsFillAirplaneFill className="icon sidebar_insidef" />
-              <div className="item_tite sidebar_insidef">Products</div>
-            </div>
-            <div className="sidebar_item">
-              <BsFillAirplaneFill className="icon sidebar_insidef" />
-              <div className="item_tite sidebar_insidef">Products</div>
-            </div>
+      {open ? null : (
+        <div className="sidebar_inside">
+          <div className="sidebar_item">
+            <BsFillAirplaneFill className="icon sidebar_insidef" />
+            <div className="item_tite sidebar_insidef">Products</div>
           </div>
-          }
-       <div className="sidebar_item">
+          <div className="sidebar_item">
+            <BsFillAirplaneFill className="icon sidebar_insidef" />
+            <div className="item_tite sidebar_insidef">Products</div>
+          </div>
+        </div>
+      )}
+      <div className="sidebar_item">
         <BsFillAirplaneFill className="icon" />
         <div className="item_tite">Products</div>
       </div>
- 
 
       <div className="sidebar_item">
         <BsFillAirplaneFill className="icon" />
@@ -56,7 +67,7 @@ const Sidebar = () => {
         <BsFillAirplaneFill className="icon" />
         <div className="item_tite">Tables</div>
       </div>
- 
+
       <div className="sidebar_item">
         <BsFillAirplaneFill className="icon" />
         <div className="item_tite">Seeting</div>
